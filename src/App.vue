444be5c46cabd63311fb9a2d69e96d677adc4bba
tepbuphatakim <kimtepbuphata009@gmail.com>
2024-08-22 17:36:05 +0700

    fix: ts vue type

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
-<script lang="ts">
-import alchemy from './utils/alchemy.js'
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  data() {
-    return {
-      blockNumber: null as number | null
-    }
-  },
-  async created() {
-    this.blockNumber = await alchemy.core.getBlockNumber()
-  }
-})
-</script>
-
 <template>
-  {{ blockNumber }}
-  <!-- <RouterView /> -->
+  <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+import alchemy from '@/utils/alchemy'
+
+export default defineComponent({
+  data() {
+    return {
+      blockNumber: null as number | null
+    }
+  },
+  async created() {
+    this.blockNumber = await alchemy.core.getBlockNumber()
+  }
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  {{ blockNumber }}
 </template>
